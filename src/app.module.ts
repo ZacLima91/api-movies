5d13e8cbbb403565acc457c416f8ae2613ac8548
jadielson <jajadielsonmanoel@gmail.com>
2023-01-25 00:50:41 -0300

    config module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { MoviesModule } from './movies/movie.module';
+import { PrismaModule } from './prisma/prisma.module';
 import { UsersModule } from './user/users.module';
 
 @Module({
-  imports: [UsersModule, MoviesModule],
+  imports: [UsersModule, MoviesModule, PrismaModule],
 })
 export class AppModule {}
