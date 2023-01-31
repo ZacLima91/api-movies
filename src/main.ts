de7e388ea12eceede44dd5035e835c8e099dd948
jadielson <jajadielsonmanoel@gmail.com>
2023-01-31 19:13:35 -0300

    config cors error

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
-import cors from 'cors';
+import * as cors from 'cors';
 
 
 async function bootstrap() {
